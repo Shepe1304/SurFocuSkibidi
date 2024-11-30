b9cc5a8733b44290ac5c5338c4b94e2d1bd56786
Shepe1304 <hqgiang1346@gmail.com>
2024-11-30 01:00:51 -0500

    Added duo screen to main page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import ExtendableDiv from "./components/ExtendableDiv";
 import React, { useState } from "react";
 import Header from "./homepage/header/Header";
 import Footer from "./homepage/footer/Footer";
+import { gifList } from "./assets/GifList";
 
 function App() {
+  const [brainrot, setBrainrot] = useState(false);
+  const [randomizedVideo, setRandomizedVideo] = useState(0);
+
   return (
-    <div className="App">
-      <Header />
-      <ExtendableDiv
-        text={
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem neque officia incidunt adipisci tempora inventore nostrum magnam veritatis consequatur aliquam, tenetur optio quis magni iusto possimus voluptatem fugit sequi vitae."
-        }
-      />
-      <Footer />
+    <div className="App--container">
+      <div className={`App ${brainrot ? "App--brainrot_resize" : ""}`}>
+        <button
+          onClick={() => {
+            setBrainrot(!brainrot);
+            setRandomizedVideo(Math.floor(Math.random() * gifList.length));
+          }}
+        >
+          {!brainrot ? "HELP ME FOCUS!" : "STOP ITTT"}
+        </button>
+        <Header />
+        <ExtendableDiv
+          text={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem neque officia incidunt adipisci tempora inventore nostrum magnam veritatis consequatur aliquam, tenetur optio quis magni iusto possimus voluptatem fugit sequi vitae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem neque officia incidunt adipisci tempora inventore nostrum magnam veritatis consequatur aliquam, tenetur optio quis magni iusto possimus voluptatem fugit sequi vitae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem neque officia incidunt adipisci tempora inventore nostrum magnam veritatis consequatur aliquam, tenetur optio quis magni iusto possimus voluptatem fugit sequi vitae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem neque officia incidunt adipisci tempora inventore nostrum magnam veritatis consequatur aliquam, tenetur optio quis magni iusto possimus voluptatem fugit sequi vitae."
+          }
+        />
+        <Footer />
+      </div>
+      {brainrot ? (
+        <div className="App--brainrot_container">
+          <div className="App--brainrot">
+            <img src={gifList[randomizedVideo].link} alt="" />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
